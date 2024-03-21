7f0c9e49a2b6201f3e617ebe66d91744525e8426
Nekembe Boris <nekembeb@gmail.com>
2024-03-21 23:36:50 +0000

    refactor db library

--- backend-flask/lib/db.py
+++ backend-flask/lib/db.py
@@ -1,7 +1,25 @@
 from psycopg_pool import ConnectionPool
 import os
 
-def query_wrap_object(template):
+
+class Db:
+  def __init__(self):
+    self.init_pool()
+
+  def init_pool(self):
+    connection_url = os.getenv("CONNECTION_URL")
+    self.pool = ConnectionPool(connection_url)
+
+  def query_commit(self)
+    try:
+      conn = self.pool.connection()
+      cur = conn.cursor()
+      cur.execute(sql)
+      conn.commit()
+    except Exception as error:
+      conn.rollback()
+  
+  def query_wrap_object(self, template):
   sql = f"""  
   (SELECT COALESCE(row_to_json(object_row),'{ {}}'::json) FROM (
   {template}
@@ -9,13 +27,35 @@ def query_wrap_object(template):
 
   return sql
 
-def query_wrap_array(template):
-  sql = f"""
-  (SELECT COALESCE(array_to_json(array_agg(row_to_json(array_row))),'[]'::json) FROM (
-  {template}
-  ) array_row);
-  """
-  return sql
+  def query_wrap_array(self, template):
+    sql = f"""
+    (SELECT COALESCE(array_to_json(array_agg(row_to_json(array_row))),'[]'::json) FROM (
+    {template}
+    ) array_row);
+    """
+    return sql
 
-connection_url = os.getenv("CONNECTION_URL")
-pool = ConnectionPool(connection_url)
+
+  def query_array_json(self, sql):
+    wrapped_sql = self.query_wrap_array(sql)
+    with self.pool.connection() as conn:
+      with conn.cursor() as cur:
+        cur.execute(wrapped_sql)
+        # this will return a tuple
+        # the first field being the data
+        json = cur.fetchone()
+        return json[0]    
+
+
+  def query_object_josn(self, sql):
+    wrapped_sql = self.query_wrap_object(sql)
+    with self.pool.connection() as conn:
+      with conn.cursor() as cur:
+        cur.execute(wrapped_sql)
+        # this will return a tuple
+        # the first field being the data
+        json = cur.fetchone()
+        return json[0]
+
+
+db = Db()
