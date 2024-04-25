e0554fe3f3686b57500572ccbd1bd760239b5370
Nekembe Boris <nekembeb@gmail.com>
2024-04-25 22:20:19 +0000

    correcting user_handle

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -1,13 +1,13 @@
 -- this file was manually created
 INSERT INTO public.users (display_name, handle, email, cognito_user_id)
 VALUES
-  ('Nekembe', 'Xerxes', 'borispokwangeh@email', 'MOCK'),
+  ('Nekembe', 'xerxes', 'borispokwangeh@email', 'MOCK'),
   ('Andrew Bayko', 'bayko', 'test@email' ,'MOCK');
 
 INSERT INTO public.activities (user_uuid, message, expires_at)
 VALUES
   (
-    (SELECT uuid FROM public.users WHERE users.handle = 'Xerxes' LIMIT 1),
+    (SELECT uuid FROM public.users WHERE users.handle = 'xerxes' LIMIT 1),
     'This was imported as seed data!',
     current_timestamp + interval '10 day'
   )
