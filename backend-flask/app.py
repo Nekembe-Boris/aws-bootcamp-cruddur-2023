3ae3468556a98614efc1b4211909e5d5570744e1
Nekembe Boris <nekembeb@gmail.com>
2024-05-20 02:38:26 +0000

    implementing new user

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -6,6 +6,7 @@ import os
 #server side cognito auth
 from lib.cognito_ver_token import CognitoJWTToken, extract_access_token, TokenVerifyError
 
+from services.users_short import *
 from services.home_activities import *
 from services.notifications_activities import *
 from services.user_activities import *
@@ -285,6 +286,12 @@ def data_activities_reply(activity_uuid):
   else:
     return model['data'], 200
   return
+
+@app.route("/api/users/@<string:handle>/short", methods=['GET'])
+def data_users_short(handle):
+  data = UsersShort.run(handle)
+  return data, 200
+
 
 if __name__ == "__main__":
   app.run(debug=True)
